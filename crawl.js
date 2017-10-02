e4a263173a129ac9d7ae7e80c6d27773e5342fd2
Aswin Dev P S <aswin@sensomate.com>
2017-10-02 16:34:17 +0530

    Add comment
    Commented above all functions in the script.

--- crawl.js
+++ crawl.js
@@ -6,50 +6,12 @@ var fs = require('fs');
 var START_URL = "https://medium.com";
 
 var pagesToVisit = [];
-var concurrencyCount = 1;
 var requestPosition = 0;
 
-function parseBodyText(dom){
-  var relativeLinks = dom.window.document.querySelectorAll("a[href^='/']");
-  for(var i=0, loopLen=relativeLinks.length; i<loopLen; i++){
-    pagesToVisit.push(START_URL + relativeLinks[i].href);
-  }
-  var absoluteLinksHttps = dom.window.document.querySelectorAll("a[href^='https']");
-  for(var i=0, loopLen=absoluteLinksHttps.length; i<loopLen; i++){
-    pagesToVisit.push(absoluteLinksHttps[i].href);
-  }
-  var absoluteLinksHttp = dom.window.document.querySelectorAll("a[href^='http']");
-  for(var i=0, loopLen=absoluteLinksHttp.length; i<loopLen; i++){
-    pagesToVisit.push(absoluteLinksHttp[i].href);
-  }
-}
-
-console.log('\n Get all hyperlinks from : ',START_URL,'\n');
-request(START_URL, function(error, response, body) {
-    // Check status code (200 is HTTP OK)
-    if(!response){
-      console.log('Network Error');
-    }else {
-      var JSDOM = jsdom.JSDOM;
-      var dom = new JSDOM(body);
-      parseBodyText(dom);
-      console.log('\n Total '+pagesToVisit.length+' hyperlinks \n');
-      console.log('\n Trigger first five requests \n');
-      startFetching();
-    }
-});
-
-function startFetching(){
-  for (var i = 0; i < 5; i++) {
-    fireRequest(pagesToVisit[requestPosition]);
-    requestPosition++;
-  }
-}
-
+// Trigger request to url's
 
 function fireRequest(url){
   console.log('GET '+url+'\n');
-  concurrencyCount++;
   request(url, function(error, response, body) {
     if(!response){
       console.log('Network Error');
@@ -57,9 +19,8 @@ function fireRequest(url){
       console.log('\n---------------------------------------------------------------');
       console.log(response.statusCode +' Response from ' +response.request.uri.href);
       console.log('---------------------------------------------------------------');
-      concurrencyCount--;
       requestPosition++;
-      if(requestPosition < pagesToVisit.length && concurrencyCount < 6){
+      if(requestPosition < pagesToVisit.length){
         fireRequest(pagesToVisit[requestPosition]);
       }else {
         var csvOut = {
@@ -74,3 +35,47 @@ function fireRequest(url){
     }
   });
 }
+
+// Initiate first 5 requests
+
+function startFetching(){
+  for (var i = 0; i < 5; i++) {
+    fireRequest(pagesToVisit[requestPosition]);
+    requestPosition++;
+  }
+}
+
+// Parse hyperlinks from html body.
+
+function parseBodyText(dom){
+  // for relative paths
+  var relativeLinks = dom.window.document.querySelectorAll("a[href^='/']");
+  for(var i=0, loopLen=relativeLinks.length; i<loopLen; i++){
+    pagesToVisit.push(START_URL + relativeLinks[i].href);
+  }
+  // for absolute paths
+  var absoluteLinksHttps = dom.window.document.querySelectorAll("a[href^='https']");
+  for(var i=0, loopLen=absoluteLinksHttps.length; i<loopLen; i++){
+    pagesToVisit.push(absoluteLinksHttps[i].href);
+  }
+  var absoluteLinksHttp = dom.window.document.querySelectorAll("a[href^='http']");
+  for(var i=0, loopLen=absoluteLinksHttp.length; i<loopLen; i++){
+    pagesToVisit.push(absoluteLinksHttp[i].href);
+  }
+}
+
+// Fetch all hyperlinks from START_URL
+
+console.log('\n Get all hyperlinks from : ',START_URL,'\n');
+request(START_URL, function(error, response, body) {
+    if(!response){
+      console.log('Network Error');
+    }else {
+      var JSDOM = jsdom.JSDOM;
+      var dom = new JSDOM(body);
+      parseBodyText(dom);
+      console.log('\n Total '+pagesToVisit.length+' hyperlinks \n');
+      console.log('\n Trigger first five requests \n');
+      startFetching();
+    }
+});
