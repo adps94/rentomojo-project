33b27b3e8f46b284aed0cc694b32d790dc5a9271
Aswin Dev P S <aswin@sensomate.com>
2017-10-02 14:22:16 +0530

    Webpage crawler script
    This project contains crawling script for fetching all hyperlinks present in  https://medium.com . 5 requests are continuesly fired to first 5 links. When one request is resolved, next one will triggered. This keep concurrency 5. Script execute untill all hyperlinks are exhausted.

---
+++ README.md
@@ -0,0 +1,51 @@
+# Project Title
+
+Webpage Crawler
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development purposes.
+
+### Prerequisites
+
+  ```
+  Install NodeJS and NPM
+  ```
+
+### Installing
+
+  ```
+  git clone https://github.com/adps94/rentomojo-project.git
+  cd rentomojo-project
+  npm install
+  ```
+
+### Running
+
+  Run project in normal nodejs script
+
+  ```
+  npm start
+  ```
+
+  Run project using async library
+
+  ```
+  git checkout -b async
+  git pull origin async
+  npm install
+  npm start
+  ```
+
+## Built With
+
+* [Node.JS](https://nodejs.org/) - JavaScript run-time environment for executing JavaScript code server-side.
+
+
+## Authors
+
+* **Aswin Dev P S** - [Rentomojo Project](https://github.com/adps94/rentomojo-project.git)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ crawl.js
@@ -0,0 +1,73 @@
+var request = require('request');
+var jsdom = require("jsdom");
+var csvWriter = require('csv-write-stream');
+var fs = require('fs');
+
+var START_URL = "http://www.arstechnica.com";
+
+var pagesToVisit = [];
+var concurrencyCount = 1;
+var requestPosition = 0;
+
+console.log('\n Get all hyperlinks from : ',START_URL,'\n');
+request(START_URL, function(error, response, body) {
+    // Check status code (200 is HTTP OK)
+    if(!response){
+      console.log('Network Error');
+    }else {
+      var JSDOM = jsdom.JSDOM;
+      var dom = new JSDOM(body);
+      var relativeLinks = dom.window.document.querySelectorAll("a[href^='/']");
+      for(var i=0, loopLen=relativeLinks.length; i<loopLen; i++){
+        pagesToVisit.push(START_URL + relativeLinks[i].href);
+      }
+      var absoluteLinksHttps = dom.window.document.querySelectorAll("a[href^='https']");
+      for(var i=0, loopLen=absoluteLinksHttps.length; i<loopLen; i++){
+        pagesToVisit.push(absoluteLinksHttps[i].href);
+      }
+      var absoluteLinksHttp = dom.window.document.querySelectorAll("a[href^='http']");
+      for(var i=0, loopLen=absoluteLinksHttp.length; i<loopLen; i++){
+        pagesToVisit.push(absoluteLinksHttp[i].href);
+      }
+      console.log('\n Total '+pagesToVisit.length+' hyperlinks \n');
+      console.log('\n Fire first five requests \n');
+      startFetching(); 
+    }
+});
+
+function startFetching(){
+  for (var i = 0; i < 5; i++) {
+    fireRequest(pagesToVisit[requestPosition]);
+    requestPosition++;
+  }
+}
+
+
+function fireRequest(url){
+  console.log('GET '+url+'\n');
+  concurrencyCount++;
+  request(url, function(error, response, body) {
+    if(!response){
+      console.log('Network Error');
+    } else {
+      console.log('\n---------------------------------------------------------------');
+      console.log(response.statusCode +' Response from ' +response.request.uri.href);
+      console.log('---------------------------------------------------------------');
+      concurrencyCount--;
+      requestPosition++;
+      if(requestPosition < pagesToVisit.length && concurrencyCount < 6){
+        fireRequest(pagesToVisit[requestPosition]);
+      }else {
+        var csvOut = {
+          hyperlinks : pagesToVisit
+        };
+        var writer = csvWriter();
+        writer.pipe(fs.createWriteStream('output.csv'));
+        writer.write(csvOut);
+        writer.end();
+        return;
+      }
+    }
+  });
+}
+
